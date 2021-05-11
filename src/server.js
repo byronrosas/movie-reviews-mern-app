0bedb5caaa516bf8cca820c8e148b82328d83318
Byron Rosas <byron_mma@hotmail.es>
2021-05-11 11:23:18 -0500

    fixed for production

--- src/server.js
+++ src/server.js
@@ -82,7 +82,7 @@ app.use('/API/review', reviewRouter);
 //  handle error
 app.use((err,req,res,next)=>{
     res.locals.message = err.message;
-    res.locals.error = process.env.ENV === 'development' ? err : {};
+    res.locals.error = process.env.ENV === 'production' ? err : {};
 
     res.status(err.status || 500);
 
