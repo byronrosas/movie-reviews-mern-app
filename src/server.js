1d7a1cb37dffdf2c0bc7753d606eff24f659c98b
Byron Rosas <byron_mma@hotmail.es>
2021-05-11 11:15:29 -0500

    fixed for production

--- src/server.js
+++ src/server.js
@@ -18,6 +18,7 @@ const app = express();
 // white origin list
 var whiteList = [
     process.env.CLIENT_URL,
+    'http://moviemern-app.herokuapp.com',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:3001',
     'http://localhost:3001',
